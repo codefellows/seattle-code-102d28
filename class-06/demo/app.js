504d63b9189dfc079de7d920c13b17e9676f5150
Roger Huba <rogerhuba@gmail.com>
2021-06-30 17:00:28 -0500

    add class 6 demo

---
+++ class-06/demo/app.js
@@ -0,0 +1,15 @@
+// console.log('Hello World!!!!!!!!!');
+
+// let userName = 'Roger';
+// let userAge = 45;
+
+// console.log(userName)
+// console.log(userAge)
+
+// userAge = 21
+// console.log(userAge)
+
+let user_name = prompt('Please Enter Your Name ');
+console.log(user_name)
+document.write('Welcome ' + user_name + '!')
+// <h2>Welcome Roger!</h2>
